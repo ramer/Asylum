7822fff587e014aca8789aab591283defe9d5585
ramer <ramer.mobile@gmail.com>
2019-05-31 10:21:56 +0300

    Добавьте файлы проекта.

---
+++ Asylum/Asylum.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Asylum.ino
+ Created:	31.05.2019 10:21:52
+ Author:	sorokovikov
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
